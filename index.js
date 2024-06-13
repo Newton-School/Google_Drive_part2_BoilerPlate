b2a33a2a680d89af034e554f2c8ab46e8ebba01e
DeepeshNST <deepesh.agrawal@newtonschool.co>
2024-06-13 15:04:53 +0530

    initial boiler plate

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require("path")
+const fs = require("fs")
+
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+// POST endpoint to fetch directory and files
+app.get('/public*', (req, res) => {
+
+});
+
+
+// POST endpoint to create folder
+app.post('/createFolder', (req, res) => {
+
+});
+
+
+// POST endpoint to rename folder
+app.post('/renameFolder', (req, res) => {
+
+});
+
+
+
+
+// POST endpoint to rename file 
+app.post('/renameFile', (req, res) => {
+
+});
+
+
+
+
+
+// POST endpoint to upload files
+app.post('/uploadFile', async (req, res) => {
+
+});
+
+
+
+// Delete endpoint to delete file and folder
+app.delete('/deleteItem', async (req, res) => {
+
+
+});
+
+
+
+
+// Start the server
+app.listen(5000, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
